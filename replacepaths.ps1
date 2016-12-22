14e6b10174038895d69e39782a5e4922c781fcea
Nathan Woodrow <madmanwoo@gmail.com>
2016-12-22 10:09:45 +1000

    Add replace files script to fix no relative paths

---
+++ replacepaths.ps1
@@ -0,0 +1,12 @@
+﻿param (
+    [string]$OSGeoFolder = "OSGeo4W64"
+ )
+
+$base = $PSScriptRoot + "\" + $OSGeoFolder
+$binfolder = $base + "\bin"
+
+Get-ChildItem $binfolder -Filter *.bat | 
+Foreach-Object {
+    Write-Host "Fixing" $_.FullName
+    (Get-Content $_.FullName).replace($base, '%~do0..') | Set-Content $_.FullName
+}
